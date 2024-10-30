18e1a4f56a73d2f4e608d3a6f085fecf57ad18a8
LoveKumarAgrawal <loveagrawal204@gmail.com>
2024-10-30 13:23:36 +0530

    improved gridSize input

--- memory-game/src/components/memoryGame.tsx
+++ memory-game/src/components/memoryGame.tsx
@@ -12,11 +12,14 @@ const MemoryGame = () => {
     const [won, setWon] = useState(false)
     const [showNumberFirstTime, setShowNumberFirstTime] = useState(true);
 
-    const handleGridSize = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const size = parseInt(e.target.value)
-        setGridSize(size)
-    }
+    // const handleGridSize = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //     const size = parseInt(e.target.value)
+    //     if (size >= 2 && size <= 10) setGridSize(size)
+    // }
 
+    const handleGridSize = (size: number) => {
+        if(size >= 2 && size <= 10) setGridSize(size)
+    }
 
     const initializeGame = () => {
         const totalCards: number = gridSize * gridSize
@@ -42,7 +45,7 @@ const MemoryGame = () => {
 
     useEffect(() => {
         initializeGame();
-      }, [gridSize]);
+    }, [gridSize]);
 
 
     const checkMatch = (secondId: number) => {
@@ -82,19 +85,38 @@ const MemoryGame = () => {
     const isFlipped = (id: number) => flipped.includes(id) || solved.includes(id)
     const idSolved = (id: number) => solved.includes(id)
 
-    useEffect(()=> {
-        if(solved.length === cards.length && cards.length > 0) {
+    useEffect(() => {
+        if (solved.length === cards.length && cards.length > 0) {
             setWon(true)
         }
-    },[solved, cards])
+    }, [solved, cards])
 
     return (
+
         <div className="flex flex-col justify-center items-center min-h-screen bg-grey-100 p-4">
             <h1 className="text-3xl font-bold mb-6">Memory Game</h1>
             {/* Input */}
-            <div className="mb-4">
-                <label htmlFor="gridSize" className="mr-2">Grid Size: (Max 10)</label>
-                <input type="number" id="gridSize" min={2} max={10} value={gridSize} onChange={(e) => handleGridSize(e)} className="border-2 border-gray-300 rounded px-2 py-1" />
+            <div className="flex justify-center items-center mb-4">
+                <div>
+                <label htmlFor="gridSize" className="mr-4">Grid Size: (Max 10)</label>
+                </div>
+                <div className="relative flex items-center max-w-[8rem]">
+                    <button type="button" className="bg-gray-100 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100  focus:ring-2 focus:outline-none"
+                    onClick={() => handleGridSize(gridSize - 1)}
+                    >
+                        <svg className="w-3 h-3 text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16" />
+                        </svg>
+                    </button>
+                    <input type="text" value={gridSize} className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5"/>
+                    <button type="button" id="increment-button" className="bg-gray-100 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 focus:ring-2 focus:outline-none"
+                    onClick={() => handleGridSize(gridSize + 1)}
+                    >
+                        <svg className="w-3 h-3 text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+                        </svg>
+                    </button>
+                </div>
             </div>
 
             {/* GameBoard */}
@@ -105,9 +127,9 @@ const MemoryGame = () => {
                 }}
             >
                 {cards.map((card) => {
-                    return <div key={card.id} className={`aspect-square flex items-center justify-center text-xl font-bold rounded-lg cursor-pointer transition-all ${isFlipped(card.id) 
+                    return <div key={card.id} className={`aspect-square flex items-center justify-center text-xl font-bold rounded-lg cursor-pointer transition-all ${isFlipped(card.id)
                         ? idSolved(card.id) ? "bg-green-500 text-white"
-                        : "bg-blue-500 text-white" 
+                            : "bg-blue-500 text-white"
                         : "bg-gray-300 text-gray-400"}`}
                         onClick={() => handleClick(card.id)}
                     >
@@ -124,7 +146,7 @@ const MemoryGame = () => {
 
             {/* Button */}
             <button className="mt-4 px-4 py-2 bg-green-400 text-white rounded hover:bg-green-600 transition-colors"
-            onClick={initializeGame}
+                onClick={initializeGame}
             >
                 {won ? "Play Again" : "Reset"}
             </button>
